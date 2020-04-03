0dac006f0c7efff91bcae751927a66e70f92e533
John McMillan <john@mcmillantech.co.uk>
2020-04-03 12:26:46 +0100

    V3.2

---
+++ SudburySuffolk.php
@@ -0,0 +1,122 @@
+<?php
+// ------------------------------------------------------
+//  Project	Sudbury Chamber
+//  File	SudburySuffolk.php
+//              Visitor page
+//  
+//  Author	John McMillan, McMillan Technology
+// ------------------------------------------------------
+
+?>
+<!doctype html>
+<html>
+<head>
+
+<title>Attractions of Sudbury</title>
+<meta name="description"
+ content="Sudbury, Suffolk, England is an attactive market town surrounded by 
+ attractive countryside and classic English villages">
+<meta name="keywords" content="Sudbury, Suffolk, Market town, countryside,
+      business location"> 
+
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+<meta name=viewport content="width=device-width, initial-scale=1.0">
+<link type="text/css" rel="stylesheet" href="Chamber.css">
+<link type="text/css" rel="stylesheet" href="Menus.css">
+<script src="Chamber.js"></script> 
+<style>
+</style>
+</head>
+
+<body onLoad="checkMobile()" >
+
+<?php
+    require("Header.html");
+?>
+
+<h2>Visit Sudbury</h2>
+<img src="images/MarketHill3.jpg" style="width:880px" alt="Job Fair">
+
+<div class="span3">
+    <p>
+    Set in the heart of the Stour Valley, an area of outstanding natural beauty, the
+    market town of Sudbury is an ideal centre for both business and pleasure trips
+    to East Anglia. Sudbury is an ancient market town dating back to Saxon times.</p>
+</div>
+
+<div class="span3">
+    <p>For centuries the weaving and silk industry has prospered here and many
+    great houses and churches have been built, giving the town a major historical
+    legacy. Sudbury has been used for television locations, most significantly for
+    BBC's Lovejoy.</p>     
+</div>
+
+<div class="span3">
+    <p>It is surrounded by the attractive countryside so often painted
+    by Constable and Gainsborough and by quintessentially English villages. It is
+    an ideal base for a walking holiday.</p>
+</div>
+<div style="clear: both"> </div>
+<br>
+<div class="section2" style="height:240px;">
+    <h3>The town</h3>
+    <p>The market place is the centre of the town with many fine buildings 
+    overseen by St Peters Church, now used for many community purposes.</p>
+    <p>There are many fine restaurants, cafes and bars in town as well as banks 
+    and a mix of shops.</p>
+    <p>There is easy access to the town, with plenty of parking, up to 3 
+    hours free.</p>
+</div>
+<div class="section2" style="height:240px;">
+    <h3>Attractions</h3>
+    <p>Sudbury is the birthpace of Gainsborough. <a href="http://www.gainsborough.org/">
+    Gainborough's House</a>, his 
+    birthplace is a major gallery (currently closed for a major expansion).</p>
+    <p>Belle Vue Park is well worth a visit, with tennis courts, playgrounds
+    and a skateboard park. The Kingfisher Leisure Centre provides a swimming 
+    pool and other sporting facilities.</p>
+    <p>400 metres along Friars Street is Quay Lane, which leads to the 
+    <a href="https://www.quaysudbury.com/">Quay Theatre</a> and the River 
+    Stour Trust. In Summer weekends, there are boat 
+    trips and a tea room here.</p>
+</div>
+
+<div style="clear: both"><br> </div>
+
+<div class="section2">
+    <h3>Countryside</h3>
+    <p>The Sudbury water meadows are beautiful. They are crossed by many 
+    footpaths and are excellent for walking. Friars Meadow, closest to the town, 
+    is an ideal place for a picnic.</p>
+    <p>Next to the Kingfisher Leisure Centre, The Valley Trail starts. This runs 
+    along a disused railway as far as Long Melford. The Trail forms part of the 
+    National Cycle Network Route 13 and the St Edmunds Way.</p>
+</div>
+
+<div class="section2">
+    <h3>Business in Sudbury</h3>
+    <p>Sudbury has a long industrial tradition. It's major industry is silk -
+        95% of UK silk is woven in Sudbury</p>
+    <p>The town punches above its weight, with major companies like Siemens
+    and Ciba Corning having bases here. It has a cluster of companies that sell
+    to prestige clients, from royal palaces to world-beating guitars. From rebuilding 
+    Hampton Court and the Eurostar terminal to Highgate Cemetery.</p>
+</div>
+
+<div style="clear: both"><br> </div>
+<!--
+<div class="section2">
+    <img src="images/Charvonia.jpg" style="width:420px" alt="Chamber meeting">
+    Meeting the Chief Executive of Babergh DC
+</div>
+<div class="section2">
+    <img src="images/JobFair.jpg" style="width:377px" alt="Job Fair">
+    <br>Job fair
+</div> -->
+
+<?php
+    require("Footer.html");
+?>
+
+</body>
+</html>
